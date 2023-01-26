aa879059421d6d535e14a321833cea960ad39bd4
remek7787 <remek778790@gmail.com>
2023-01-26 16:13:58 +0100

    added 404 page & styled it, installed tsparticles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage';
+import NoPage404 from './pages/NoPage404';
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path='/' element={<MainPage />} />
+        <Route path='*' element={<NoPage404 />} />
       </Routes>
     </>
   );
