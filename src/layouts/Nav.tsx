3de8a82573bf67faff32c2ae2f9cdf26c12d0d12
remek7787 <remek778790@gmail.com>
2023-02-01 14:48:02 +0100

    fixed parallax

--- src/layouts/Nav.tsx
+++ src/layouts/Nav.tsx
@@ -1,5 +1,14 @@
 const Nav = () => {
-  return <nav>Nav</nav>;
+  return (
+    <nav className='sticky top-0 right-0 w-full py-3'>
+      <div className='absolute top-0 left-0 w-full h-full bg-red-200 opacity-20' />
+      <ul className='text-white flex flex-row'>
+        <li className='px-2'>Nav1</li>
+        <li className='px-2'>Nav2</li>
+        <li className='px-2'>Nav3</li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,74 +1,97 @@
 import Main from '../layouts/Main';
-import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import { IParallax, Parallax, ParallaxLayer } from '@react-spring/parallax';
+import Nav from '../layouts/Nav';
+import { useRef } from 'react';
 
 const MainPage = () => {
+  const parallax = useRef<IParallax>(null!);
   return (
     <div>
       <Main>
-        <Parallax pages={4} className='max-h-[350vh]'>
-          <ParallaxLayer
-            offset={1}
-            speed={1}
-            className='flex items-center justify-center bg-[#14222F]'
-          >
-            <div className='h-full w-full  relative'>
-              <h1 className='text-white'>Testing! 1</h1>
-            </div>
-          </ParallaxLayer>
-          <ParallaxLayer
-            offset={1.9}
-            speed={1.9}
-            className='flex items-center justify-center bg-[#14222F]'
-          >
-            <div className='h-full w-full  relative'>
-              <h1 className='text-white'>Testing! 2</h1>
-            </div>
-          </ParallaxLayer>
-          <ParallaxLayer
-            offset={3}
-            speed={3}
-            className='flex items-center justify-center bg-[#14222F]'
-          >
-            <div className='h-full w-full  relative'>
-              <h1 className='text-white'>Auction</h1>
-            </div>
-          </ParallaxLayer>
-          <ParallaxLayer
-            offset={3.9}
-            speed={3.9}
-            className='flex items-center justify-center bg-[#14222F]'
-          >
-            <div className='h-full w-full  relative'>
-              <h1 className='text-white'>Footer</h1>
-            </div>
-          </ParallaxLayer>
-
+        <Parallax ref={parallax} pages={6} className='bg-[#14222F]'>
           <ParallaxLayer
             offset={0}
             speed={0}
             className='flex items-center justify-center bg-red-100 z-0'
           >
-            <div className='h-full w-full  relative'>
-              <h1>Hello</h1>
+            <div className=' relative'>
+              <h1>Its a Parallax</h1>
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer
+            offset={0}
+            sticky={{ start: 0, end: 6 }}
+            speed={1}
+            factor={0.05}
+            className='sticky top-0 left-0'
+          >
+            <Nav />
+          </ParallaxLayer>
+          <ParallaxLayer
+            offset={1}
+            speed={1}
+            factor={1}
+            className='relative flex items-center justify-center'
+            onClick={() => parallax.current.scrollTo(2)}
+          >
+            <div className='absolute top-0 left-0 h-full w-full bg-[#72FFF4] opacity-[2%]' />
+            <div className='relative'>
+              <h1 className='text-white'>It's a shop</h1>
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer
+            offset={2}
+            factor={0.95}
+            speed={1}
+            className='relative flex items-center justify-center'
+          >
+            <div className='absolute top-0 left-0 h-full w-full bg-[#72FFF4] opacity-[2%]' />
+            <div className='relative'>
+              <h1 className='text-white'>Its a gallery</h1>
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer
+            offset={3}
+            speed={1}
+            factor={0.95}
+            className='relative flex items-center justify-center'
+          >
+            <div className='absolute top-0 left-0 h-full w-full bg-[#72FFF4] opacity-[2%]' />
+            <div className='relative'>
+              <h1 className='text-white'>Its a Auction</h1>
+            </div>
+          </ParallaxLayer>
+          <ParallaxLayer
+            offset={4}
+            speed={1}
+            factor={0.95}
+            className='relative flex items-center justify-center'
+          >
+            <div className='absolute top-0 left-0 h-full w-full bg-[#72FFF4] opacity-[2%]' />
+            <div className='relative'>
+              <h1 className='text-white'>Its a Online books</h1>
             </div>
           </ParallaxLayer>
 
           <ParallaxLayer
-            offset={2}
-            speed={2}
-            className='flex items-center justify-center bg-[#14222F]'
+            offset={5}
+            speed={1}
+            factor={0.9}
+            className='relative flex items-center justify-center'
           >
-            <div className='h-full w-full  relative text-white'>
-              <h1 className='text-white'>Its a 2</h1>
+            <div className='absolute top-0 left-0 h-full w-full bg-[#72FFF4] opacity-[2%]' />
+            <div className='relative '>
+              <h1 className='text-white'>Its a Contact</h1>
             </div>
           </ParallaxLayer>
           <ParallaxLayer
-            offset={2.9}
-            speed={2.9}
-            className='flex items-center justify-center bg-[#14222F]'
+            offset={5.9}
+            speed={1}
+            className='relative flex items-start justify-center bg-[#111D28]'
           >
-            <div className='h-full w-full  relative'>
-              <h1 className='text-white'>Its a 2.9</h1>
+            <div className='absolute top-0 left-0 h-full w-full' />
+            <div className='relative '>
+              <h1 className='text-white'>Its a footer</h1>
             </div>
           </ParallaxLayer>
         </Parallax>
