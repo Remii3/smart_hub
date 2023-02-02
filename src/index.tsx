89a98cb6476a21f5de5e722630da666e7d23e311
Remigiusz Woźniak <remek778790@gmail.com>
2023-02-02 22:10:12 +0100

    quick routes fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './styles/parallax.css';
+import './assets/styles/parallax.css';
 
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
