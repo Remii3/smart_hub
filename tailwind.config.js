9803a4e6ae82bfc4fd69e8c7168b836a395638e2
remek7787 <remek778790@gmail.com>
2023-02-03 12:46:52 +0100

    fixed tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  mode: 'jit',
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
