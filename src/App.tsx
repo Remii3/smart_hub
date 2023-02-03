289f1db23908b0c88ac453d0204f9faa4055efdb
remek7787 <remek778790@gmail.com>
2023-02-03 12:51:25 +0100

    code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
 import { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-
 import SuspenseComponent from './components/suspense/SuspenseComponent';
 import LoadingComponent from './components/UI/LoadingComponent';
 
 const MainPage = lazy(() => import('./pages/MainPage'));
 
 function App() {
-
   return (
     <>
-
       <SuspenseComponent fallback={<LoadingComponent />}>
         <Routes>
           <Route path='/' element={<MainPage />} />
         </Routes>
       </SuspenseComponent>
-
     </>
   );
 }
