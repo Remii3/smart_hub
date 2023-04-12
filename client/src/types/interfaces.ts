1595566e03bf39d41c8bafa6c83d64984058359a
Remigiusz Woźniak <remek778790@gmail.com>
2023-04-12 14:08:19 +0200

    styled carousels, added category model

---
+++ client/src/types/interfaces.ts
@@ -0,0 +1,14 @@
+export interface BookTypes {
+  _id: number;
+  title: string;
+  authors: string[];
+  categories: string[];
+  volume: string;
+  edition: string;
+  language: string;
+  pages: number;
+  url: string;
+  description: string;
+  cover_url: string;
+  price: number;
+}
