3cd3a9ef9da639ddd65c62f64bb756ee96e32f8a
Remigiusz Woźniak <remek778790@gmail.com>
2023-04-13 13:59:35 +0200

    url check

--- api/Routes/account.routes.js
+++ api/Routes/account.routes.js
@@ -3,7 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 const checkAuth = require('../Middleware/checkAuth.middleware');
-
 const {
   signIn,
   signUp,
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,17 +13,16 @@ const MyAccount = lazy(() => import('./pages/MyAccount'));
 let properUrl = 'http://localhost:4000';
 
 switch (document.URL) {
-  case 'http://localhost:5173':
+  case 'http://localhost:5173/':
     properUrl = 'http://localhost:4000';
     break;
-  case 'https://smarthub-jb8g.onrender.com':
+  case 'https://smarthub-jb8g.onrender.com/':
     properUrl = 'https://smarthub-backend.onrender.com';
     break;
   default:
     properUrl = 'https://smarthub-backend.onrender.com';
     break;
 }
-
 axios.defaults.baseURL = properUrl;
 axios.defaults.withCredentials = true;
 
